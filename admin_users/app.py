706de906a42f5ac1d1586931749219b9eb6c8cd6
netscro <kovalev_evgeniy@list.ru>
2021-01-23 18:54:11 +0200

    add user to admin

--- admin_users/app.py
+++ admin_users/app.py
@@ -4,25 +4,52 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from flask_security import UserMixin, RoleMixin
-
+from flask_security import UserMixin, RoleMixin, SQLAlchemyUserDatastore, Security, login_required
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
+# path to admin database
 db_path = os.path.join(os.path.dirname(__file__), 'admin_users.db')
 db_uri = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 
 app.config['SECRET_KEY'] = 'dfgefge2354234fsdfwefwefsdf23432wefwd'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 admin = Admin(app, template_mode='bootstrap3')
+
+
+# Flask-Security-Too
+
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(100), unique=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
+    roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(255))
+
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+admin.add_view(ModelView(User, db.session))
 
 
 @app.route('/')
